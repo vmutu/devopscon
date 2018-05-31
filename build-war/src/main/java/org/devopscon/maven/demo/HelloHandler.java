ea00c8b1d66d6891cd51a51db8e215a72326b65a
Valeriu <valeriumutu@gmail.com>
2018-05-31 16:31:27 +0200

    Updated the message

--- build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.devopscon.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hello DevOpsCon, What do you think about this WAR file?";
+		String hello = "Hello DevOpsCon, What do you think about this 2018 conference?";
 		return hello;
 	}
 
